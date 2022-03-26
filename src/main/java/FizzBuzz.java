6ac65f3af72d6579e921337fd335b1f69cd9c05f
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-03-26 15:54:00 +0000

    Feature: added FizzBuzz class

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,15 @@
+public class FizzBuzz {
+    private int number;
+
+    public FizzBuzz(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
