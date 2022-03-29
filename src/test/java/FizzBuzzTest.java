a3cbf4e9ca0aa5a698ce8ad62d45d0b20da21b92
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-03-29 17:22:59 +0100

    Refactor: change all test for FizzBuzz

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,18 +1,51 @@
 import org.junit.Test;
 import wrapper.FizzBuzzInt;
+import wrapper.FizzBuzzString;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
-    FizzBuzz newFizzBuzz;
+    FizzBuzzString newFizzBuzzString;
 
     @Test
-    public void shouldBeAny() {
-        assertEquals("1", new FizzBuzz(new FizzBuzzInt(1)).whatIs().toString());
-        assertEquals("Fizz", new FizzBuzz(new FizzBuzzInt(9)).whatIs().toString());
-        assertEquals("Buzz", new FizzBuzz(new FizzBuzzInt(10)).whatIs().toString());
-        assertEquals("FizzBuzz", new FizzBuzz(new FizzBuzzInt(15)).whatIs().toString());
-        assertEquals("Fizz", new FizzBuzz(new FizzBuzzInt(13)).whatIs().toString());
-        assertEquals("Buzz", new FizzBuzz(new FizzBuzzInt(59)).whatIs().toString());
+    public void shouldBeNumberString(){
+        newFizzBuzzString = new FizzBuzz(new FizzBuzzInt(1)).whatIs();
+
+        assertEquals("1", newFizzBuzzString.toString());
+    }
+
+    @Test
+    public void shouldBeFizz(){
+        newFizzBuzzString = new FizzBuzz(new FizzBuzzInt(3)).whatIs();
+
+        assertEquals("Fizz", newFizzBuzzString.toString());
+    }
+
+    @Test
+    public void shouldBeBuzz(){
+        newFizzBuzzString = new FizzBuzz(new FizzBuzzInt(5)).whatIs();
+
+        assertEquals("Buzz", newFizzBuzzString.toString());
+    }
+
+    @Test
+    public void shouldBeFizzBuzz(){
+        newFizzBuzzString = new FizzBuzz(new FizzBuzzInt(15)).whatIs();
+
+        assertEquals("FizzBuzz", newFizzBuzzString.toString());
+    }
+
+    @Test
+    public void shouldBeBuzzBecauseContainsThree() {
+        newFizzBuzzString = new FizzBuzz(new FizzBuzzInt(13)).whatIs();
+
+        assertEquals("Fizz", newFizzBuzzString.toString());
+    }
+
+    @Test
+    public void shouldBeBuzzBecauseContainsFive() {
+        newFizzBuzzString = new FizzBuzz(new FizzBuzzInt(59)).whatIs();
+
+        assertEquals("Buzz", newFizzBuzzString.toString());
     }
 }
